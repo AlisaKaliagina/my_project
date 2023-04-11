b91054550360325afd30956229b53ad8c4a03436
AlisaKaliagina <122281368+AlisaKaliagina@users.noreply.github.com>
2023-04-11 20:28:57 +0200

    dada

---
+++ streamlit/new_bot.py
@@ -0,0 +1,12 @@
+import streamlit as st
+st.title("Тест")
+st.write("""Тестовое приложение""")
+
+st.sidebar.title("About")
+st.sidebar.info(
+    """
+    This app is Open Source dashboard.
+    """
+)
+st.sidebar.info("#Тест "
+                "Тест")

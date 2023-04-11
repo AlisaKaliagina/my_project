e44f8a058a82b13eddb9f58b3198ffa3635574c2
AlisaKaliagina <122281368+AlisaKaliagina@users.noreply.github.com>
2023-04-11 20:57:43 +0200

    dadada

--- streamlit/app.py
+++ streamlit/app.py
@@ -70,3 +70,29 @@ if st.button("Найти продукты"):
     else:
         st.write(filtered_products)
 
+
+st.title("AllergenNix")
+st.write("""Your beautytool """)
+st.image('skin-jumbo-v2.gif')
+
+st.sidebar.title("Info")
+st.sidebar.info(
+    """
+    This app is Open Source dashboard.
+    """
+)
+st.sidebar.info("#Тест "
+                "Тест")
+# Загружаем новые оптимизированные данные
+DATA = ('products_new_df.csv')
+@st.cache # для оптимизации работы приложения
+
+# Создадим функцию для загрузки данных
+def load_data():
+    df = pd.read_csv(DATA)
+    return df   
+
+# Применим функцию 
+df = load_data()
+st.write (df)
+

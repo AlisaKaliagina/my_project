05a667beb448fc556264a86f3ee33d274fbc93a6
AlisaKaliagina <122281368+AlisaKaliagina@users.noreply.github.com>
2023-04-12 01:19:40 +0200

    we

--- streamlit/AllergenNix.py
+++ streamlit/AllergenNix.py
@@ -8,7 +8,7 @@ st.image('skin-jumbo-v2.gif')
 st.sidebar.title("How to use?")
 
 with st.sidebar.beta_expander("How can I determine my skin type?"):
-    st.write("[**Here**](https://www.luminskin.com/blog/556068306980/how-to-determine-your-skin-type?utm_source=google&utm_medium=cpc&utm_campaign=16950478023&utm_term=&utm_content=pmax&gclid=CjwKCAjwitShBhA6EiwAq3RqA-LiB1Bt9Hbz6BfG8X8_D1WZ_u8JFopotWKduwAdW_aKtLbtyZN1hhoC85MQAvD_BwE) you can find some information")
+    st.write("[**Here**](https://www.luminskin.com/blog/556068306980/how-to-determine-your-skin-type?utm_source=google&utm_medium=cpc&utm_campaign=16950478023&utm_term=&utm_content=pmax&gclid=CjwKCAjwitShBhA6EiwAq3RqA-LiB1Bt9Hbz6BfG8X8_D1WZ_u8JFopotWKduwAdW_aKtLbtyZN1hhoC85MQAvD_BwE) you can find some information.Keep in mind that skin type can change over time due to various factors, such as age, hormones, climate, or lifestyle. It's essential to reassess your skin type periodically and adjust your skincare routine accordingly. If you're unsure about your skin type or have concerns, it's always best to consult with a dermatologist")
     st.image("31SKIN-superJumbo.gif")
 
 with st.sidebar.beta_expander("Contact"):

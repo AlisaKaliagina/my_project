a84e45df8a5ea00a7ebbfe4e839c15e3b935e306
AlisaKaliagina <122281368+AlisaKaliagina@users.noreply.github.com>
2023-04-11 23:47:43 +0200

    new

--- streamlit/new_bot.py
+++ streamlit/new_bot.py
@@ -15,7 +15,7 @@ with st.sidebar.beta_expander("Learn more about allergens and ingredients"):
     st.write("Of course any part of the body can show an allergic reaction to cosmetic products. But, usually the more sensitive areas are those where the skin is thinner and more sensitive, such as the face, neck, décolletage and inner arms.")
     st.image("31SKIN-superJumbo.gif")
 
-st.sidebar.info("Contact")
+st.sidebar.info("Contact with me")
 
 # Загружаем новые оптимизированные данные
 DATA = ('products_new_df.csv')

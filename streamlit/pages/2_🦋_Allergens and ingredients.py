80cc41bfcc6220a27f395f114267c114d637c1ad
AlisaKaliagina <122281368+AlisaKaliagina@users.noreply.github.com>
2023-04-12 11:39:48 +0200

    aller_text

--- streamlit/pages/2_🦋_Allergens and ingredients.py
+++ streamlit/pages/2_🦋_Allergens and ingredients.py
@@ -1,13 +1,15 @@
 import streamlit as st
 
 
-st.subheader('Allergens and Ingredients in Cosmetics')
+st.title('Allergens and Ingredients in Cosmetics')
 st.write(
     """
     Cosmetics contain a variety of ingredients, some of which can be potential allergens. Allergic reactions can occur when the immune system overreacts to a particular ingredient, causing symptoms such as itching, redness, and swelling.
 
 Common allergens in cosmetics include fragrances, preservatives, and certain dyes. It's important to note that just because an ingredient is natural or organic doesn't mean it's necessarily safe for everyone.
-
+    """)
+st.subheader(
+    """
 ❗ Some natural ingredients can also cause allergic reactions.❗
     """
 )
@@ -16,13 +18,15 @@ st.image('clean_beauty.jpeg')
 st.write(
     """
 To prevent allergic reactions, it's important to read the labels and ingredient lists of cosmetics carefully. If you have a known allergy to a particular ingredient, avoid products that contain it. If you're unsure about a particular ingredient, do a patch test before using the product on your entire face or body.
-
+    """)
+st.subheader(
+    """
 🚨 If you do experience an allergic reaction to a cosmetic product, stop using it immediately and seek medical attention if the symptoms are severe or persistent.🚨
     """
 )
 
 
-st.subheader('Allergen Awareness')
+st.title('Allergen Awareness')
 
 
 st.image('allergens-TITOLO.png')

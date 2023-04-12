c9991e1e561882aec08054e8741e5fdb186c4c17
AlisaKaliagina <122281368+AlisaKaliagina@users.noreply.github.com>
2023-04-12 11:30:32 +0200

    emojis

--- streamlit/pages/3_🔮_About.py
+++ streamlit/pages/3_🔮_About.py
@@ -1,30 +1,30 @@
 import streamlit as st
 
-st.title(':blue[Analyzing allergens in cosmetics comes with several limitations and challenges, some of which are:]')
+st.title('Analyzing allergens in cosmetics comes with several limitations and challenges, some of which are:')
 
 st.image('cosm_about.jpeg')
 
-st.subheader('1. Incomplete ingredient lists')
+st.subheader('1. Incomplete ingredient lists 🕵🏼 :')
 
 st.write(
     """
 Some manufacturers may not disclose the complete list of ingredients on the product label, making it difficult to identify potential allergens.
 
     """)
-st.subheader('2. Varying individual reactions:')
+st.subheader('2. Varying individual reactions 🧚‍♀️ :')
 st.write(
     """
 Allergic reactions are highly individual, and a substance that causes a reaction in one person may not cause the same reaction in another. This makes it challenging to determine whether a specific ingredient is an allergen for everyone.
     """
 )
-st.subheader('3. Complex chemical compositions: ')
+st.subheader('3. Complex chemical compositions 🧬 : ')
 
 st.write(
     """
 Many cosmetic products have complex chemical compositions, making it difficult to pinpoint the exact allergen responsible for an adverse reaction.
     """
 )
-st.subheader('4. Allergen thresholds:')
+st.subheader('4. Allergen thresholds 🧪:')
 
 st.write(
     """
@@ -32,26 +32,19 @@ Some individuals may only experience an allergic reaction when exposed to a cert
     """
 )
 
-st.subheader('5. Lack of standardized testing:')
+st.subheader('5. Lack of standardized testing 🔬:')
 
 st.write(
     """
 There is no universally accepted method for testing cosmetic products for allergens, making it difficult to compare the allergenic potential of different products.
 """)
 
-st.subheader('6. Changes in product formulations:')
+st.subheader('6. Changes in product formulations 🔄 :')
 
 st.write(
     """
 Manufacturers may change product formulations over time, introducing new allergens or increasing the concentration of existing allergens. This makes it difficult to maintain up-to-date information about the allergenic potential of specific products.
 """)
-st.subheader('7. Regulatory differences: ')
-
-st.write(
-    """
-Different countries have different regulations regarding allergens and ingredient labeling, leading to inconsistencies in the information available to consumers.
-""")
-
 
 st.subheader(
     """
